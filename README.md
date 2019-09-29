1235ac4a44f126b0b7cc886b99d0ca2806caf94f
artemkv <artemkv@gmail.com>
2019-09-29 16:45:07 +0200

    k8s cluster

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 1. Create a bucket named "journey-250514-tf-state" to store the state.
 2. Create a service account with the following roles:
 
+    - Compute Instance Admin (v1)
+    - Kubernetes Engine Admin
     - Cloud Datastore Owner
     - Security Admin
     - Service Account Admin
+    - Service Account User
     - Pub/Sub Admin
     - Storage Admin
 
--- main.tf
+++ main.tf
@@ -96,3 +96,35 @@ resource "google_pubsub_topic_iam_binding" "events-service-publish-to-error" {
         "serviceAccount:${google_service_account.events-service-account.email}"
     ]
 }
+
+# TODO: split into a separate module
+
+# Create GKE cluster
+resource "google_container_cluster" "gke_cluster" {
+  name     = "gke-cluster"
+  location = "${var.gcp_region}"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count = 1
+}
+
+# Create GKE cluster node pool
+resource "google_container_node_pool" "gke_cluster_node_pool" {
+  name       = "gke-cluster-node-pool"
+  location   = "${var.gcp_region}"
+  cluster    = "${google_container_cluster.gke_cluster.name}"
+  node_count = 1 # per zone
+
+  node_config {
+    preemptible  = false
+    machine_type = "f1-micro"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
